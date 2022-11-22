67ef551dc5ee8fe82c4ac3cc28cc1311e8a0c41b
MAAghakabiri <a.aghakabiri@gmail.com>
2022-11-23 01:03:40 +0330

    Add reviewRepo and profile controller

---
+++ IMDB/IMDB.Domain/CardViewModel/ProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IMDB.DataLayer.Model;
+using static IMDB.Domain.DTOs.MovieDTO;
+
+namespace IMDB.Domain.CardViewModel
+{
+    public class ProfileViewModel
+    {
+        public User User { get; set; }
+        public List<Movie> WatchList { get; set; }
+    }
+}
