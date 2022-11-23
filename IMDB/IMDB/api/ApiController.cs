be4fedfc3ba2c5258e41240605ae5996b5d50ce5
Shams <mahdishams.13811381@gmail.com>
2022-11-23 16:47:11 +0330

    api get one video add

---
+++ IMDB/IMDB/api/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMDB.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly IMovie _movie;
+        public ApiController(IMovie movie)
+        {
+            _movie = movie;
+        }
+        public async Task<IActionResult> getOneVideoKey(int id)
+        {
+            var videos = await _movie.GetVideoById(id);
+
+            return Ok(videos.Results.LastOrDefault().Key);
+        }
+    }
+}
