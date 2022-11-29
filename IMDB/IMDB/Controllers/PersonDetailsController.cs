2ceb9138739bca4f7b45cea9446c5eee7e2d1b61
MAAghakabiri <a.aghakabiri@gmail.com>
2022-11-30 01:35:35 +0330

    PersonDetails View and Controller

---
+++ IMDB/IMDB/Controllers/PersonDetailsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMDB.Controllers
+{
+    public class PersonDetailsController : Controller
+    {
+        private IMovie _movie { get;}
+        public PersonDetailsController(IMovie movie)
+        {
+            _movie = movie;
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            var details = await _movie.GetPersonDetailes(id);
+            return View(details);
+        }
+    }
+}
