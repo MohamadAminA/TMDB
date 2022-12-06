c089c3c26bcee18cf3f606f2f811bfd65554f47c
M-Shams <mahdishams.13811381@gmail.com>
2022-12-06 16:51:43 +0330

    fix

---
+++ IMDB/IMDB.DataLayer/Migrations/20221205214343_DESKTOP-0QSKDOG.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IMDB.DataLayer.Migrations
+{
+    public partial class DESKTOP0QSKDOG : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
